004ceb429c2031fd91bb5b46c227df5784e367c4
st10454790 <st10454790@myemeris.edu.za>
2026-09-10 09:15:53 +0200

    basic landing page and tips and advice

---
+++ XLBondsKZN-Website/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XLBondsKZN_Website.Models;
+
+namespace XLBondsKZN_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult TipsAndAdvice()
+        {
+            return View("~/Views/Advice&Tips/Tips and Advice.cshtml");
+        }
+    }
+}
